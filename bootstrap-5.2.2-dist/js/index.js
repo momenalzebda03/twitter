edbf971bb961705c09594816d2cdde1c0ad0f72e
unknown <momen07996@gmail.com>
2023-10-04 22:12:57 +0300

    end project

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -4,48 +4,45 @@ const refreshButton = document.getElementById("refreshButton");
 const likeButton = document.getElementById("likeButton");
 const createButton = document.getElementById("createButton");
 
-function displayFruits() {
-  forloopDiv.innerHTML = "";
-  for (let i = 0; i < fruits.length; i += 2) {
-    const tweetContainer = document.createElement("div");
-    const newParagraph = document.createElement("p");
-    const tweetValue = fruits[i];
-    const nameValue = fruits[i + 1];
-    const displayText = `Name: ${nameValue}<br>Tweet: ${tweetValue}`;
+function createTweetContainer(tweet, name) {
+  const tweetContainer = document.createElement("div");
+  tweetContainer.className = "tweet-container";
+  const newParagraph = document.createElement("p");
+  const displayText = `Name: <span class='name-span d-none'>${name}</span><br>Tweet: ${tweet}`;
+  newParagraph.innerHTML = displayText;
+  newParagraph.className = "mb-0 tagText text-white p-3 rounded-3 fw-bold mt-3";
+  const buttonContainer = document.createElement("div");
+  buttonContainer.className = "d-flex justify-content-between gap-2 w-100 mt-2";
 
-    newParagraph.innerHTML = displayText;
-    newParagraph.className =
-      "mb-0 tagText text-white p-3 rounded-3 fw-bold mt-3";
+  const refreshButtonClone = refreshButton.cloneNode(true);
+  refreshButtonClone.addEventListener("click", function () {
+    createTweetContainer(tweet, name);
+  });
 
-    const buttonContainer = document.createElement("div");
-    buttonContainer.className =
-      "d-flex justify-content-between gap-2 w-100 mt-2";
+  const likeButtonClone = likeButton.cloneNode(true);
+  likeButtonClone.addEventListener("click", function () {
+    const arrayColor = ["red", "black", "white", "orange", "gold", "green"];
+    const randomColorIndex = Math.floor(Math.random() * arrayColor.length);
+    const randomColor = arrayColor[randomColorIndex];
+    const paragraph = this.closest(".tweet-container").querySelector("p");
+    paragraph.style.setProperty("color", randomColor, "important");
+  });
 
-    const refreshButtonClone = refreshButton.cloneNode(true);
-    refreshButtonClone.addEventListener("click", function () {
-      // Handle refresh button click
-    });
+  const createButtonClone = createButton.cloneNode(true);
+  createButtonClone.addEventListener("click", function () {
+    const nameSpan = tweetContainer.querySelector(".name-span");
+    if (nameSpan) {
+      nameSpan.classList.toggle("d-none");
+    }
+  });
 
-    const likeButtonClone = likeButton.cloneNode(true);
-    likeButtonClone.addEventListener("click", function () {
-      // Handle like button click
-    });
-
-    const createButtonClone = createButton.cloneNode(true);
-    createButtonClone.addEventListener("click", function () {
-      // Handle create button click
-    });
-
-    buttonContainer.appendChild(refreshButtonClone);
-    buttonContainer.appendChild(likeButtonClone);
-    buttonContainer.appendChild(createButtonClone);
-    tweetContainer.appendChild(newParagraph);
-    tweetContainer.appendChild(buttonContainer);
-    forloopDiv.appendChild(tweetContainer);
-  }
+  buttonContainer.appendChild(refreshButtonClone);
+  buttonContainer.appendChild(likeButtonClone);
+  buttonContainer.appendChild(createButtonClone);
+  tweetContainer.appendChild(newParagraph);
+  tweetContainer.appendChild(buttonContainer);
+  forloopDiv.appendChild(tweetContainer);
 }
-
-displayFruits();
 
 const form = document.getElementById("myForm");
 const tweetInput = document.getElementById("tweetInput");
@@ -57,8 +54,8 @@ form.addEventListener("submit", function (event) {
   const newTweetName = nameInput.value.trim();
   if (newTweet !== "" && newTweetName !== "") {
     fruits.push(newTweet, newTweetName);
+    createTweetContainer(newTweet, newTweetName);
     console.log("Updated Array:", fruits);
-    displayFruits();
     tweetInput.value = "";
     nameInput.value = "";
   }
